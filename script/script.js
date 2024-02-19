d12b69688a2efd001b3c6a143e181f38900a0f9b
Tobias Fernandes <tobys312@gmail.com>
2024-02-19 18:48:14 -0300

    Better responsive layout for 320px width devices

--- script/script.js
+++ script/script.js
@@ -1,4 +1,12 @@
 const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".container-header");
+const contentHeader = document.querySelector(".content-header");
 
-hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+hamburguer.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    contentHeader.classList.toggle("active");
+});
+
+// Using JQuery
+
+
